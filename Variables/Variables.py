d70fb50721b2cd0cdb6e8f0f4d6e065322679be9
Adwiti789 <adwiti1975@gmail.com>
2018-06-06 22:54:23 -0700

    String slicing

---
+++ Variables/Variables.py
@@ -0,0 +1,20 @@
+parrot = "norwegian blue"
+print(parrot)  # this will print the whole norwegian blue
+print(parrot[3])  # it will print the fourth character w
+print(parrot[-1])  # last character output will  be e
+print(parrot[0:4])  # this will print norw
+print(parrot[:4])  # norw
+print(parrot[4:]) # it will print all the characters from 5th character egian blue
+print(parrot[-4:-2]) # it will print characters from last starting from the 4th character from last till 2nd character which is bl
+print(parrot[0:6:2]) # starting from the first character till the 5th character print letters with interval 2 nre
+number = '9,223,345,567,789'
+print(number[1::4]) # this will print all the ,
+numbers = '1,2,3,4,5,6,7'
+print(numbers[0::2]) # 1234567
+print(numbers[0:13:2]) #1234567
+print("hello " *5) # hello hello hello hello hello
+
+today ="Wednesday"
+print("day" in today) # true
+print("Wed" in today) # true
+
