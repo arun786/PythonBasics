1fbb6216ca936d31fe2beb21a33b1314e4b96ba1
Adwiti789 <adwiti1975@gmail.com>
2018-06-07 23:35:16 -0700

    For loop

---
+++ FlowControl/ForLoop.py
@@ -0,0 +1,10 @@
+for i in range(1, 12):
+    print('No. {:2} when squared {:3} and cubed {:4}'.format(i, i ** 2, i ** 3))
+
+for i in range(10, 12):
+    print(i)
+
+print('-------')
+a = 12
+for i in range(1, 12):
+    print(i + a)
