199b717b05d80069a1ebadfa1142c716e48c985a
Adwiti789 <adwiti1975@gmail.com>
2018-06-12 22:50:35 -0700

    for loop

---
+++ FlowControl/ForLoopDetail.py
@@ -0,0 +1,8 @@
+number = '1,2,3,4,567,789,21,90,34'
+for i in range(0, len(number)):
+    print(number[i], end='')
+print()
+print('-----------------')
+for i in range(0, len(number)):
+    if number[i] in '0123456789':
+        print(number[i], end='')
