b0a76310d78bd3f2cf4cc13d4890eb60b2997f48
Adwiti789 <adwiti1975@gmail.com>
2018-06-19 21:49:22 -0700

    range

---
+++ SequenceTypes/RangeExplained/Range.py
@@ -0,0 +1,20 @@
+my_list = list(range(10))
+print(my_list)  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+even = list(range(0, 10, 2))
+odd = list(range(1, 10, 2))
+
+print(even)  # [0, 2, 4, 6, 8]
+print(odd)  # [1, 3, 5, 7, 9]
+
+my_list = 'abcdefghijklmnopqrstuvwxyz'
+print(my_list.index('e'))  # 4
+print(my_list[4])  # e
+
+small_decimals = range(0, 10)
+print(small_decimals)  # range(0,10)
+
+print(small_decimals.index(3))  # 3
+
+odd = range(1, 10000, 2)
+print(odd[985])  # 1971
